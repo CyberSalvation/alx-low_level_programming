364e7e309df3965cdc4af8a0e0786485af23948c
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 11:38:00 +0200

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@ int main(void)
 	/* Message to be printed to stderr */
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	/* Using the write system call to print to stderr */
-	write(STDERR_FILENO, message, 52); /* 52 is the length of the message */
+	write(STDERR_FILENO, message, 59); /* 52 is the length of the message */
 	return (1); /* Return 1 as required */
 }
