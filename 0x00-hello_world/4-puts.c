a5db90ef84dab1be274ec5c3ce2e2afc7f2a839f
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 09:33:48 +0200

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: Prints a quote and returns 0
  * return: Always 0 (Sucess)
  */
-int main(void)
+int main(void)//Descripyion: Prints a quote and returns 0
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
