47b77b9d55aecfff0c49919aa1f161db7f4be013
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 10:00:12 +0200

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point of the program
  *
- * Description: Prints a quote and returns 0
+ * Description: Prints a quote using puts and returns 0 upon success.
  *
- * return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
-int main(void) /* Prints a quote and returns 0 */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /* pr */
-	/* Indicates successful execution */
-	return (0); /* returns 0 */
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }
