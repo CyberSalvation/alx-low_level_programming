cffff46c68856819999ef9e8294d198dae60132c
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 09:25:37 +0200

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void) {
+
+/**
+ * main - entry point
+ *
+ * return: Always 0 (Sucess)
+ */	
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
