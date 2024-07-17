5b81588f0158f39ea122d40804628707a1ed30de
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 10:17:05 +0200

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,12 @@
  * Description: Prints a quote and returns 0 (Success)
  *
  * Return: Always 0 (Success)
- * */
+ */
+
 int main(void)
 {
-	printf("%s%s%s%s%s", "with proper grammar, but the outcome is a piece of art,", "\n", "", "", "\n");
+	printf("%s%s%s%s%s",
+			"with proper grammar, but the outcome is a piece of art,",
+			"\n", "", "", "\n");
 	return (0);
 }
