bd9f50d834f2a725cb4f8efc28f00a2b7a331b8b
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 11:50:22 +0200

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
 #include <unistd.h>
-
+/**
+ * this code doese the following
+ *
+ * Dersctipiton: main description
+ *
+ * Return: retuns 1 (Success)
+ *
+ */
 int main(void)
 {
 	/* Message to be printed to stderr */
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	/* Using the write system call to print to stderr */
-	write(STDERR_FILENO, message, 59); /* 52 is the length of the message */
+	write(STDERR_FILENO, msg, 59); /* 52 is the length of the message */
 	return (1); /* Return 1 as required */
 }
