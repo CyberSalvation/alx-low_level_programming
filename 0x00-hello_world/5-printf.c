1cc4549a7f6985fa854478897213ed2fedd7685f
Wandile Salvation Tobela <salvation.tobela@gmail.com>
2024-07-17 10:11:57 +0200

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote and returns 0 (Success)
+ *
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	printf("%s%s%s%s%s", "with proper grammar, but the outcome is a piece of art,", "\n", "", "", "\n");
+	return (0);
+}
